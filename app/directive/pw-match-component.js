67614ca6a75c6d2e9534674093da8675ebe1885c
FazioD <faziod@uw.edu>
2016-09-06 20:46:03 -0700

    removing console log and white space

--- app/directive/pw-match-component.js
+++ app/directive/pw-match-component.js
@@ -3,23 +3,13 @@
 const angular = require('angular');
 angular.module('ramble')
 .directive('pwCheck', function () {
-  console.log('directive found!!!');
   return {
     require: 'ngModel',
     link: function (scope, elem, attrs, ctrl, $setValidity) {
       scope.$watch(attrs['pwCheck'], function (errorMsg) {
-        console.log('elem', elem[0]);
-        console.log('input', );
         elem[0].setCustomValidity(errorMsg);
         console.log('errorMsg', errorMsg);
         ctrl.$setValidity('pwCheck', errorMsg ? false : true);
-        // if(scope.signupForm.$dirty) {
-        //   console.log('scope.signForm.$dirty', scope.signForm.$dirty);
-        // } else {
-        //   ctrl.$setPristine();
-        // }
-
-        console.log('$', $setValidity);
       });
     }
   };
